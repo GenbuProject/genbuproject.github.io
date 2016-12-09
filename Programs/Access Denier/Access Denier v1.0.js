924104c78b99206d0b2ac66c67b461a264fa0c9c
Genbu Hase <hasegenbu@gmail.com>
2016-12-09 16:28:23 +0900

    完成かな...?

--- Programs/Access Denier/Access Denier v1.0.js
+++ Programs/Access Denier/Access Denier v1.0.js
@@ -1,16 +1,20 @@
 (function () {
 	let Error = document.getElementsByTagName("ErrorCode");
 	
-	if (Error.length != 0 && Error.attributes["Href"]) {
-		var CodeListGetter = new XMLHttpRequest();
-			CodeListGetter.open("GET", Error.attributes["Href"].value, false);
-			
-			CodeListGetter.onload = function (Event) {
-				if (Error.attributes["Value"]) {
-					location.href = JSON.parse(CodeListGetter.response)[parseInt(Error.attributes["Value"].value)];
-				}
+	if (Error.length != 0) {
+		for (let i = 0; i < Error.length; i++) {
+			if (Error[i].attributes["Href"]) {
+				var CodeListGetter = new XMLHttpRequest();
+					CodeListGetter.open("GET", Error[i].attributes["Href"].value, false);
+					
+					CodeListGetter.onload = function (Event) {
+						if (Error[i].attributes["Value"]) {
+							location.href = JSON.parse(CodeListGetter.response)[parseInt(Error[i].attributes["Value"].value)];
+						}
+					}
+					
+					CodeListGetter.send(null);
 			}
-			
-			CodeListGetter.send(null);
+		}
 	}
 })();
