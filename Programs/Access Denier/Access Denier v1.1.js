6e6d97611800b9030a5b208027f4b7499e4e4b37
Genbu Hase <hasegenbu@gmail.com>
2016-12-21 08:22:43 +0900

    ちょっとアクセス権限を強化。

---
+++ Programs/Access Denier/Access Denier v1.1.js
@@ -0,0 +1,38 @@
+location.querySort = function () {
+	var Querys = {};
+	
+	for (var i = 0; i < location.search.substr(1).split("&").length; i++) {
+		Querys[location.search.substr(1).split("&")[i].split("=")[0].toUpperCase()] = location.search.substr(1).split("&")[i].split("=")[1];
+	}
+	
+	return Querys;
+}
+
+(function () {
+	let Error = document.getElementsByTagName("ErrorCode");
+	
+	if (Error.length != 0) {
+		for (let i = 0; i < Error.length; i++) {
+			if (Error[i].attributes["Href"]) {
+				var CodeListGetter = new XMLHttpRequest();
+					CodeListGetter.open("GET", Error[i].attributes["Href"].value, false);
+					
+					CodeListGetter.onload = function (Event) {
+						if (Error[i].attributes["Value"]) {
+							if (Error[i].attributes["ID"] && Error[i].attributes["Password"]) {
+								let Querys = location.querySort();
+								
+								if (Querys.ID != Error[i].attributes["ID"].value && Querys.PASSWORD != Error[i].attributes["Password"].value) {
+									location.href = JSON.parse(CodeListGetter.response)[Error[i].attributes["Value"].value];
+								}
+							} else {
+								location.href = JSON.parse(CodeListGetter.response)[Error[i].attributes["Value"].value];
+							}
+						}
+					}
+					
+					CodeListGetter.send(null);
+			}
+		}
+	}
+})();
