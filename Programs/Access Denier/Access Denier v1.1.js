9f79113740dee4198b3cc3c876b1955600422af3
Genbu Hase <hasegenbu@gmail.com>
2016-12-21 08:38:24 +0900

    エラー修正。

--- Programs/Access Denier/Access Denier v1.1.js
+++ Programs/Access Denier/Access Denier v1.1.js
@@ -1,12 +1,14 @@
-location.querySort = function () {
-	var Querys = {};
-	
-	for (var i = 0; i < location.search.substr(1).split("&").length; i++) {
-		Querys[location.search.substr(1).split("&")[i].split("=")[0].toUpperCase()] = location.search.substr(1).split("&")[i].split("=")[1];
+(function () {
+	location.querySort = function () {
+		var Querys = {};
+		
+		for (var i = 0; i < location.search.substr(1).split("&").length; i++) {
+			Querys[location.search.substr(1).split("&")[i].split("=")[0].toUpperCase()] = location.search.substr(1).split("&")[i].split("=")[1];
+		}
+		
+		return Querys;
 	}
-	
-	return Querys;
-}
+})();
 
 (function () {
 	let Error = document.getElementsByTagName("ErrorCode");
