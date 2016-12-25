da1e37f5f9a4b37ebe59ba6c90289bce2f6e50b5
Genbu Hase <hasegenbu@gmail.com>
2016-12-25 17:15:24 +0900

    Finder.jsを作成。

---
+++ Content/Specifics/YouFinder/Script/Finder.js
@@ -0,0 +1,47 @@
+let Util = {
+	CreateDialog: function (Title, Content, FooterContent) {
+		let DialogBack = document.createElement("Div");
+			DialogBack.className = "DialogBack";
+			
+		let DialogFront = document.createElement("Div");
+			DialogFront.className = "DialogFront";
+			
+		let DialogName = document.createElement("Name");
+			DialogName.textContent = Title;
+			
+		let DialogSeparater = document.createElement("Hr");
+		
+		let DialogContent = document.createElement("Content");
+			DialogContent.innerHTML = Content;
+			
+		let DialogFooter = document.createElement("Footer");
+		
+		document.body.appendChild(DialogBack);
+		document.body.appendChild(DialogFront);
+		
+		DialogFront.appendChild(DialogName);
+		DialogFront.appendChild(DialogSeparater);
+		DialogFront.appendChild(DialogContent);
+		DialogFront.appendChild(DialogFooter);
+		
+		DialogFooter.innerHTML = FooterContent;
+		
+		DialogFront.style.top = (document.documentElement.clientHeight - DialogFront.clientHeight) / 2 + "px";
+		DialogFront.style.left = (document.documentElement.clientWidth - DialogFront.clientWidth) / 2 + "px";
+	},
+	
+	DismissDialog: function () {
+		document.getElementsByClassName("DialogBack")[0].parentElement.removeChild(document.getElementsByClassName("DialogBack")[0]);
+		document.getElementsByClassName("DialogFront")[0].parentElement.removeChild(document.getElementsByClassName("DialogFront")[0]);
+	},
+	
+	QuerySort: function () {
+		let Querys = {};
+		
+		for (var i = 0; i < location.search.substr(1).split("&").length; i++) {
+			Querys[location.search.substr(1).split("&")[i].split("=")[0].toUpperCase()] = location.search.substr(1).split("&")[i].split("=")[1];
+		}
+		
+		return Querys;
+	}
+}
