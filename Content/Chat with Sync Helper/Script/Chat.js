7a5122525eb249ffaa27dade554e62fe854cdba2
Genbu Hase <hasegenbu@gmail.com>
2017-01-01 15:16:06 +0900

    タイトルの入力を強制に。

--- Content/Chat with Sync Helper/Script/Chat.js
+++ Content/Chat with Sync Helper/Script/Chat.js
@@ -99,12 +99,16 @@ const Util = {
 
 function Init() {
 	document.getElementById("Accept").addEventListener("click", function () {
-		Util.Send(document.getElementById("Title").value, document.getElementById("Content").value, document.getElementById("Name").value, new Date().toLocaleString());
-		
-		document.getElementsByClassName("BackFog")[0].style.display = "None";
-		document.getElementById("Form").children[1].style.display = "None";
-		
-		alert("コメントを投稿しました。")
+		if (document.getElementById("Name").value == "" || document.getElementById("Content").value == "") {
+			alert("入力必須項目が入力されていません。");
+		} else {
+			Util.Send(document.getElementById("Title").value, document.getElementById("Content").value, document.getElementById("Name").value, new Date().toLocaleString());
+			
+			document.getElementsByClassName("BackFog")[0].style.display = "None";
+			document.getElementById("Form").children[1].style.display = "None";
+			
+			alert("コメントを投稿しました。")
+		}
 	});
 	
 	document.getElementById("FormBtn").addEventListener("click", function () {
