af96d4b106cbeb7e7eddb4f49b7f407dd68693d0
Genbu Hase <hasegenbu@gmail.com>
2017-01-04 18:45:06 +0900

    Googleログイン実装。

--- Content/SimpleThread/Script/Main.js
+++ Content/SimpleThread/Script/Main.js
@@ -27,17 +27,16 @@
 		return Memory;
 	}
 	
-	window.DOM.XHR = function (Type, URL, DoesSync, Data) {
+	window.DOM.XHR = function (Args) {
 		let Connector = new XMLHttpRequest();
-			Connector.open(Type, URL, DoesSync);
+			Connector.open(Args.Type, Args.URL, Args.DoesSync);
 			
-			Connector.onload = function () {
+			Connector.onload = function (Event) {
+				Args.OnLoad(Event);
 				return Connector;
 			}
 			
-		(Data ? function () {
-			Connector.send(Data)
-		} : function () {})();
+			Connector.send(Args.Data);
 	}
 	
 	window.location.querySort = function () {
@@ -58,11 +57,10 @@ let Res = {
 		ClientID: atob("NjQ2NTI3MzA2ODAzLXFjNzc4MnVoZDg1NTZpb2hpZzc4dDJvdmNuNWd1Y21kLmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29t"),
 		SecretID: atob("X0RMUWkyM1o5NDMwRlJHUlhPVlF6SXNj"),
 		
-		RedirectURL: "https://genbuproject.github.io/Content/SimpleThread/",
+		RedirectURL: "http://localhost:3141/", //"https://genbuproject.github.io/Content/SimpleThread/",
 		Scope: "https://www.googleapis.com/auth/plus.login+https://www.googleapis.com/auth/plus.me",
 		
-		Token: "",
-		Code: ""
+		Token: ""
 	}
 }
 
@@ -70,16 +68,55 @@ const Net = {
 	Google: {
 		Login: function () {
 			let URL = "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&access_type=offline&approval_prompt=force&client_id=" + Res.Google.ClientID + "&redirect_uri=" + Res.Google.RedirectURL + "&scope=" + Res.Google.Scope;
-			location.href = URL;
+			
+			Res.LoginPopup = window.open(URL, "_self", "Width=" + document.documentElement.clientWidth / 2 + ", Height=" + document.documentElement.clientHeight / 1.5 + ", Left=" + (document.documentElement.clientWidth - document.documentElement.clientWidth / 2) / 2 + ", Top=" + (document.documentElement.clientHeight - document.documentElement.clientHeight / 1.5) / 2);
 		},
 		
 		Getter: {
 			Token: function () {
-				DOM.XHR("POST", "https://www.googleapis.com/oauth2/v4/token?access_type=offline&grant_type=authorization_code&client_id=" + Res.Google.ClientID + "&client_secret=" + Res.Google.SecretID + "&redirect_uri=" + Res.Google.RedirectURL + "&code=" + Res.Google.Code, true);
+				DOM.XHR({
+					Type: "POST",
+					URL: "https://www.googleapis.com/oauth2/v4/token?access_type=offline&grant_type=authorization_code&client_id=" + Res.Google.ClientID + "&client_secret=" + Res.Google.SecretID + "&redirect_uri=" + Res.Google.RedirectURL + "&code=" + location.querySort().CODE,
+					DoesSync: true,
+					
+					OnLoad: function (Event) {
+						Res.Google.Token = JSON.parse(Event.target.response);
+					}
+				});
 			},
 			
 			OpenID: function () {
-				DOM.XHR("GET", "https://www.googleapis.com/plus/v1/people/me/openIdConnect?access_token=" + Res.Google.Token, true);
+				DOM.XHR({
+					Type: "GET",
+					URL: "https://www.googleapis.com/plus/v1/people/me/openIdConnect?access_token=" + Res.Google.Token,
+					DoesSync: true,
+					
+					OnLoad: function (Event) {
+						console.log(Event);
+					}
+				});
+			}
+		},
+		
+		Sender: {
+			SaveToGDrive: function (Args) {
+				let ResData = [
+					"====================",
+					"Content-Type: Application/Json",
+					"",
+					JSON.stringify({
+						title: Args.FileName,
+						mimeType: Args.ContentType
+					}),
+					"====================",
+					"Content-Type: " + Args.ContentType,
+					"Content-Transfer-Encoding: base64",
+					"",
+					btoa(unescape(encodeURIComponent(Args.Content))),
+					"===================="
+				].join("\r\n").slice(1, -1);
+				
+				console.log(ResData);
 			}
 		}
 	}
