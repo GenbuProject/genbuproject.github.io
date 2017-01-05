df74600db118b3f1e0e01857b8edcf069f5d8206
Genbu Hase <hasegenbu@gmail.com>
2017-01-05 18:04:56 +0900

    大幅変更。
    1. Storageを使ってログイン状態を保持するように。
    2. Google Driveと連携し、リフレッシュトークンを保持するように。
    3. 45分に1度トークンを取得するように。

--- Content/SimpleThread/Script/Main.js
+++ Content/SimpleThread/Script/Main.js
@@ -29,10 +29,29 @@
 	
 	window.DOM.XHR = function (Args) {
 		let Connector = new XMLHttpRequest();
-			Connector.open(Args.Type, Args.URL, Args.DoesSync);
+			
+			Args.Params ? (function () {
+				let Param = [];
+				
+				for (let ParamName in Args.Params) {
+					Param.push(ParamName + "=" + Args.Params[ParamName]);
+				}
+				
+				Connector.open(Args.Type ? Args.Type : "GET", (Args.URL ? Args.URL : location.href) + "?" + Param.join("&"), Args.DoesSync ? Args.DoesSync : true);
+			})() : (function () {
+				Connector.open(Args.Type ? Args.Type : "GET", (Args.URL ? Args.URL : location.href), Args.DoesSync ? Args.DoesSync : true);
+			})();
+			
+			Args.Headers ? (function () {
+				for (let HeaderName in Args.Headers) {
+					Connector.setRequestHeader(HeaderName, Args.Headers[HeaderName]);
+				}
+			})() : (function () {
+				
+			})();
 			
 			Connector.onload = function (Event) {
-				Args.OnLoad(Event);
+				(Args.OnLoad ? Args.OnLoad(Event) : null);
 				return Connector;
 			}
 			
@@ -58,29 +77,83 @@ let Res = {
 		SecretID: atob("X0RMUWkyM1o5NDMwRlJHUlhPVlF6SXNj"),
 		
 		RedirectURL: "http://localhost:3141/", //"https://genbuproject.github.io/Content/SimpleThread/",
-		Scope: "https://www.googleapis.com/auth/plus.login+https://www.googleapis.com/auth/plus.me",
 		
-		Token: ""
+		Scope: [
+			"https://www.googleapis.com/auth/plus.login",
+			"https://www.googleapis.com/auth/plus.me",
+			"https://www.googleapis.com/auth/drive",
+			"https://www.googleapis.com/auth/drive.appdata"
+		],
+		
+		AccessToken: "",
+		RefreshToken: ""
 	}
 }
 
 const Net = {
 	Google: {
 		Login: function () {
-			let URL = "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&access_type=offline&approval_prompt=force&client_id=" + Res.Google.ClientID + "&redirect_uri=" + Res.Google.RedirectURL + "&scope=" + Res.Google.Scope;
+			let URL = "https://accounts.google.com/o/oauth2/v2/auth?response_type=code&access_type=offline&approval_prompt=force&client_id=" + Res.Google.ClientID + "&redirect_uri=" + Res.Google.RedirectURL + "&scope=" + Res.Google.Scope.join("+");
 			
-			Res.LoginPopup = window.open(URL, "_self", "Width=" + document.documentElement.clientWidth / 2 + ", Height=" + document.documentElement.clientHeight / 1.5 + ", Left=" + (document.documentElement.clientWidth - document.documentElement.clientWidth / 2) / 2 + ", Top=" + (document.documentElement.clientHeight - document.documentElement.clientHeight / 1.5) / 2);
+			window.open(URL, "_self", "Width=" + document.documentElement.clientWidth / 2 + ", Height=" + document.documentElement.clientHeight / 1.5 + ", Left=" + (document.documentElement.clientWidth - document.documentElement.clientWidth / 2) / 2 + ", Top=" + (document.documentElement.clientHeight - document.documentElement.clientHeight / 1.5) / 2);
 		},
 		
 		Getter: {
+			Signed: function () {
+				if (sessionStorage.getItem("AccessToken")) {
+					return true;
+				} else {
+					return false;
+				}
+			},
+			
 			Token: function () {
 				DOM.XHR({
 					Type: "POST",
-					URL: "https://www.googleapis.com/oauth2/v4/token?access_type=offline&grant_type=authorization_code&client_id=" + Res.Google.ClientID + "&client_secret=" + Res.Google.SecretID + "&redirect_uri=" + Res.Google.RedirectURL + "&code=" + location.querySort().CODE,
-					DoesSync: true,
+					URL: "https://www.googleapis.com/oauth2/v4/token",
+					DoesSync: false,
+					
+					Params: {
+						"access_type": "offline",
+						"grant_type": "authorization_code",
+						
+						"client_id": Res.Google.ClientID,
+						"client_secret": Res.Google.SecretID,
+						"redirect_uri": Res.Google.RedirectURL,
+						"code": location.querySort().CODE
+					},
 					
 					OnLoad: function (Event) {
-						Res.Google.Token = JSON.parse(Event.target.response);
+						Res.Google.AccessToken = JSON.parse(Event.target.response).access_token;
+						
+						sessionStorage.setItem("AccessToken", JSON.parse(Event.target.response).access_token);
+						
+						Net.Google.Filer.SaveAppData({
+							Name: "RefreshToken",
+							ContentType: "text/plain",
+							
+							Content: JSON.parse(Event.target.response).refresh_token
+						});
+					}
+				});
+			},
+			
+			Refresh: function () {
+				DOM.XHR({
+					Type: "POST",
+					URL: "https://www.googleapis.com/oauth2/v4/token",
+					DoesSync: true,
+					
+					Params: {
+						"grant_type": "refresh_token",
+						
+						"client_id": Res.Google.ClientID,
+						"client_secret": Res.Google.SecretID,
+						"refresh_token": Net.Google.Filer.GetAppData("RefreshToken")
+					},
+					
+					OnLoad: function (Event) {
+						console.log(Event);
 					}
 				});
 			},
@@ -88,8 +161,12 @@ const Net = {
 			OpenID: function () {
 				DOM.XHR({
 					Type: "GET",
-					URL: "https://www.googleapis.com/plus/v1/people/me/openIdConnect?access_token=" + Res.Google.Token,
+					URL: "https://www.googleapis.com/plus/v1/people/me/openIdConnect",
 					DoesSync: true,
+					
+					Params: {
+						"access_token": Res.Google.AccessToken
+					},
 					
 					OnLoad: function (Event) {
 						console.log(Event);
@@ -98,26 +175,103 @@ const Net = {
 			}
 		},
 		
-		Sender: {
-			SaveToGDrive: function (Args) {
-				let ResData = [
-					"====================",
-					"Content-Type: Application/Json",
-					"",
-					JSON.stringify({
-						title: Args.FileName,
-						mimeType: Args.ContentType
-					}),
-					"====================",
-					"Content-Type: " + Args.ContentType,
-					"Content-Transfer-Encoding: base64",
-					"",
-					btoa(unescape(encodeURIComponent(Args.Content))),
-					"===================="
-				].join("\r\n").slice(1, -1);
+		Filer: {
+			SaveAppData: function (Args) {
+				let Separator = "=====Sending=====";
 				
-				console.log(ResData);
+				DOM.XHR({
+					Type: "POST",
+					URL: "https://www.googleapis.com/upload/drive/v3/files",
+					DoesSync: true,
+					
+					Params: {
+						"uploadType": "multipart",
+						"access_token": Res.Google.AccessToken
+					},
+					
+					Headers: {
+						"Content-Type": 'multipart/mixed; boundary="' + Separator + '"'
+					},
+					
+					Data: [
+						"--" + Separator,
+						"Content-Type: application/json; charset=UTF-8",
+						"",
+						JSON.stringify({
+							name: (Args.Name ? "[" + Args.Name + "]" : "[Untitle]") + " " + (Args.Content ? Args.Content : ""),
+							
+							parents: [
+								"appDataFolder"
+							]
+						}, null, "\t"),
+						
+						"--" + Separator,
+						"Content-Type: " + (Args.ContentType ? Args.ContentType : "Text/Plain"),
+						"Content-Transfer-Encoding: UTF-8",
+						"",
+						Args.Content ? Args.Content : "",
+						"--" + Separator + "--"
+					].join("\r\n"),
+					
+					OnLoad: function (Event) {
+						
+					}
+				});
+			},
+			
+			GetAppData: function (Key) {
+				DOM.XHR({
+					Type: "GET",
+					URL: "https://www.googleapis.com/drive/v3/files",
+					DoesSync: false,
+					
+					Params: {
+						"spaces": "appDataFolder",
+						"q": "name contains '" + Key + "'",
+						"fields": "files(name)",
+						"access_token": Res.Google.AccessToken
+					},
+					
+					OnLoad: function (Event) {
+						if (JSON.parse(Event.target.response).files[0]) {
+							return JSON.parse(Event.target.response).files[0].name.split(" ")[1];
+						} else {
+							return undefined;
+						}
+					}
+				});
+			},
+			
+			IsVaild: function (Key) {
+				DOM.XHR({
+					Type: "GET",
+					URL: "https://www.googleapis.com/drive/v3/files",
+					DoesSync: false,
+					
+					Params: {
+						"spaces": "appDataFolder",
+						"q": "name contains '" + Key + "'",
+						"fields": "files(name)",
+						"access_token": Res.Google.AccessToken
+					},
+					
+					OnLoad: function (Event) {
+						if (JSON.parse(Event.target.response).files.length !== 0) {
+							return true;
+						} else {
+							return false;
+						}
+					}
+				});
 			}
+		}
+	}
+}
+
+const Util = {
+	Dialog: {
+		Init: function () {
+			
 		}
 	}
 }
@@ -126,9 +280,26 @@ let GitBase = new GitAPI(atob("YWIzNWNjODEyNDA0M2FjZmRmZmUxOTZjMGYzM2NlNjg4NzY3N
 	GitBase.Repo.RepoURL = "GenbuProject/genbuproject.github.io";
 	
 function Init() {
-	let Query = location.querySort();
-	
-	if (Query.CODE) {
-		Net.Google.Getter.Token();
+	if (!sessionStorage.getItem("AccessToken")) {
+		let Query = location.querySort();
+		
+		if (Query.CODE) {
+			DOM(":Footer")[0].className = "SignIn";
+			DOM("#Menu").children[1].className = "SignIn";
+			
+			Net.Google.Getter.Token();
+		} else {
+			DOM(":Footer")[0].className = "SignOut";
+			DOM("#Menu").children[1].className = "SignOut";
+		}
+	} else {
+		Res.Google.AccessToken = sessionStorage.getItem("AccessToken");
+		
+		DOM(":Footer")[0].className = "SignIn";
+		DOM("#Menu").children[1].className = "SignIn";
 	}
 }
+
+setInterval(function () {
+	Net.Google.Getter.Refresh();
+}, 1000 * 60 * 45);
