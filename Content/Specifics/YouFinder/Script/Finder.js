7093b0435e282666203bda138180c59ffe6a3c16
Genbu Hase <hasegenbu@gmail.com>
2017-01-07 12:42:44 +0900

    Update Finder.js

--- Content/Specifics/YouFinder/Script/Finder.js
+++ Content/Specifics/YouFinder/Script/Finder.js
@@ -1,8 +1,86 @@
+(function () {
+	window.DOM = function (Str) {
+		let Memory = null;
+		
+		switch (Str.substr(0, 1)) {
+			case "#":
+				Memory = document.getElementById(Str.substr(1));
+				break;
+				
+			case ".":
+				Memory = document.getElementsByClassName(Str.substr(1));
+				break;
+				
+			case "*":
+				Memory = document.getElementsByName(Str.substr(1));
+				break;
+				
+			case ":":
+				Memory = document.getElementsByTagName(Str.substr(1));
+				break;
+				
+			default:
+				Memory = document.createElement(Str);
+				break;
+		}
+		
+		return Memory;
+	}
+	
+	window.DOM.XHR = function (Args) {
+		let Connector = new XMLHttpRequest();
+			
+			Args.Params ? (function () {
+				let Param = [];
+				
+				for (let ParamName in Args.Params) {
+					Param.push(ParamName + "=" + Args.Params[ParamName]);
+				}
+				
+				Connector.open(Args.Type ? Args.Type : "GET", (Args.URL ? Args.URL : location.href) + "?" + Param.join("&"), Args.DoesSync ? Args.DoesSync : true);
+			})() : (function () {
+				Connector.open(Args.Type ? Args.Type : "GET", (Args.URL ? Args.URL : location.href), Args.DoesSync ? Args.DoesSync : true);
+			})();
+			
+			Args.Headers ? (function () {
+				for (let HeaderName in Args.Headers) {
+					Connector.setRequestHeader(HeaderName, Args.Headers[HeaderName]);
+				}
+			})() : (function () {
+				
+			})();
+			
+			Connector.onload = function (Event) {
+				(Args.OnLoad ? Args.OnLoad(Event) : null);
+				return Connector;
+			}
+			
+			Connector.send(Args.Data);
+	}
+	
+	
+	
+	window.DOM.width = window.innerWidth;
+	window.DOM.height = window.innerHeight;
+	
+	
+	
+	window.addEventListener("resize", function (Event) {
+		window.DOM.width = window.innerWidth;
+		window.DOM.height = window.innerHeight;
+	});
+})();
+
 let Info = {
 	ClientID: "239141875067-k7ftnrifgiv328ai7j0nnec8s79pjlro.apps.googleusercontent.com",
 	SecretID: atob("Z21COW1NOWVxVXhCOHRqNVVBSWZIeThf"),
 	RedirectURL: "https://genbuproject.github.io/Content/Specifics/YouFinder/",
-	Scope: "https://www.googleapis.com/auth/plus.login+https://www.googleapis.com/auth/plus.me+https://mail.google.com/",
+	
+	Scope: [
+		"https://www.googleapis.com/auth/plus.login”,
+		"https://www.googleapis.com/auth/plus.me”,
+		"https://mail.google.com/"
+	],
 	
 	Token: "",
 	
@@ -74,7 +152,7 @@ let Util = {
 
 let Net = {
 	LoginWithGoogle: function () {
-		location.href = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=" + Info.RedirectURL + "&scope=" + Info.Scope + "&response_type=code&client_id=" + Info.ClientID + "&access_type=offline&approval_prompt=force";
+		location.href = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=" + Info.RedirectURL + "&scope=" + Info.Scope.join("+") + "&response_type=code&client_id=" + Info.ClientID + "&access_type=offline&approval_prompt=force";
 	},
 	
 	RequestToken: function (OnLoad) {
