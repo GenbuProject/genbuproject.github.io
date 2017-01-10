ea81306e8ed9ae33dc6613a3e3d4015d541c685f
Genbu Hase <hasegenbu@gmail.com>
2017-01-10 15:42:41 +0900

    なろうのAPIじゃ本文取得出来ないのかよ...(困惑)

--- Content/Game Maker From Narou/Script/Main.js
+++ Content/Game Maker From Narou/Script/Main.js
@@ -1,5 +1,131 @@
-let Base;
+(function () {
+	window.DOM = function (Str) {
+		let Memory = null;
+		
+		switch (Str.substr(0, 1)) {
+			case "#":
+				Memory = document.getElementById(Str.substr(1));
+				break;
+				
+			case ".":
+				Memory = document.getElementsByClassName(Str.substr(1));
+				break;
+				
+			case "*":
+				Memory = document.getElementsByName(Str.substr(1));
+				break;
+				
+			case ":":
+				Memory = document.getElementsByTagName(Str.substr(1));
+				break;
+				
+			default:
+				Memory = document.createElement(Str);
+				break;
+		}
+		
+		return Memory;
+	}
+	
+	window.DOM.XHR = function (Args) {
+		let Connector = new XMLHttpRequest();
+			
+			Args.Params ? (function () {
+				let Param = [];
+				
+				for (let ParamName in Args.Params) {
+					Param.push(ParamName + "=" + Args.Params[ParamName]);
+				}
+				
+				Connector.open(Args.Type ? Args.Type : "GET", (Args.URL ? Args.URL : location.href) + "?" + Param.join("&"), Args.DoesSync ? Args.DoesSync : true);
+			})() : (function () {
+				Connector.open(Args.Type ? Args.Type : "GET", (Args.URL ? Args.URL : location.href), Args.DoesSync ? Args.DoesSync : true);
+			})();
+			
+			Args.Headers ? (function () {
+				for (let HeaderName in Args.Headers) {
+					Connector.setRequestHeader(HeaderName, Args.Headers[HeaderName]);
+				}
+			})() : (function () {
+				
+			})();
+			
+			Connector.onload = Args.OnLoad ? Args.OnLoad(Event) : null;
+			
+			Connector.send(Args.Data);
+	}
+	
+	window.DOM.JsonPXHR = function (Args) {
+		let Param = [];
+		
+		Args.Params ? (function () {
+			for (let ParamName in Args.Params) {
+				Param.push(ParamName + "=" + Args.Params[ParamName]);
+			}
+		})() : (function () {
+			
+		})();
+		
+		let Connector = new Script((Args.URL ? Args.URL : location.href) + (Args.Params ? "?" + Param.join("&") : ""));
+			
+			Connector.onload = function (Event) {
+				Connector.parentElement.removeChild(Connector);
+			}
+			
+		document.head.appendChild(Connector);
+	}
+	
+	window.Script = function (Src, Options) {
+		let Elem = document.createElement("Script");
+			Elem.src = Src ? Src : "";
+			
+		Options ? (function () {
+			Options.Async ? Elem.setAttribute("Async", "") : null;
+			Options.Defer ? Elem.setAttribute("Defer", "") : null;
+		})() : (function () {
+			
+		})();
+		
+		return Elem;
+	}
+	
+	
+	
+	window.DOM.width = window.innerWidth;
+	window.DOM.height = window.innerHeight;
+	
+	
+	
+	window.addEventListener("resize", function (Event) {
+		window.DOM.width = window.innerWidth;
+		window.DOM.height = window.innerHeight;
+	});
+})();
 
+
+
+let Base = null,
+	CallBack = function (Event) {
+		console.log(Event);
+	};
+	
 document.addEventListener("DOMContentLoaded", function (Event) {
 	Base = new RPGHelper();
+	
+	with (Base) {
+		DOM.JsonPXHR({
+			URL: "http://api.syosetu.com/novelapi/api",
+			
+			Params: {
+				"out": "jsonp",
+				"callback": "CallBack",
+				
+				"ncode": "n9137ds"
+			},
+			
+			OnLoad: CallBack
+		});
+		
+		MsgBox(R.POS.BOTTOM, R.SPEED.NORMAL, R.COLOR.WHITE, "村長\n「これで全員か......？\n　いや......まだ1人戦っておらん者が居るゾ^～」", "", function () {});
+	}
 });
