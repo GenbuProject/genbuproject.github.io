286f4e849667a515a434c42c3a8f9733f34c54c8
Genbu Hase <hasegenbu@gmail.com>
2017-01-13 19:32:56 +0900

    DOM Extender v1.1を公開。Svg関連を追加。

--- Content/Converter/AnyPictureToSvg/Script/Main.js
+++ Content/Converter/AnyPictureToSvg/Script/Main.js
@@ -24,14 +24,32 @@ document.addEventListener("DOMContentLoaded", function () {
 					let Img = new Image();
 						Img.src = Reader.result;
 						
-						Img.onload = function () {
+						Img.onload = function (Event) {
+							const Pixels = Img.getImageData().data;
+							
+							let Container = new Svg(Img.naturalWidth, Img.naturalHeight);
+							
+							for (var y = 0; y < Img.naturalHeight; y++) {
+								for (var x = 0; x < Img.naturalWidth * 4; x += 4) {
+									Container.appendChild(Svg.Rect({
+										X: x,
+										Y: y,
+										
+										Width: 1,
+										Height; 1,
+										
+										Fill: Svg.RGBA(Pixels[y * Img.naturalWidth + x], Pixels[y * Img.naturalWidth + x + 1], Pixels[y * Img.naturalWidth + x + 2], Pixels[y * Img.naturalWidth + x + 3])
+									}));
+								}
+							}
+							
 							DOM("#Message").textContent = "こ↑こ↓にファイルをドロップ";
+							
+							DOM("#Data").appendChild(Container);
 							DOM("#Data").className = "Show";
 							
 							alert("処理が完了しました。");
 						}
-						
-					DOM("#Data").appendChild(Img);
 				}
 				
 				Reader.readAsDataURL(Event.dataTransfer.files[0]);
---
+++ Programs/DOM Extender/DOM Extender v1.1.js
@@ -0,0 +1,165 @@
+/*/
+ *#######################################################################
+ *DOM Extender v1.1
+ *Copyright (C) 2016-2020 Genbu Project & Genbu Hase All Rights Reversed.
+ *#######################################################################
+/*/
+(function () {
+	window.DOM = function (Str) {
+		let Memory = null;
+		
+		switch (Str.substr(0, 1)) {
+			case "#":
+				Memory = document.getElementById(Str.substr(1));
+				break;
+				
+			case ".":
+				Memory = document.getElementsByClassName(Str.substr(1));
+				break;
+				
+			case "*":
+				Memory = document.getElementsByName(Str.substr(1));
+				break;
+				
+			case ":":
+				Memory = document.getElementsByTagName(Str.substr(1));
+				break;
+				
+			default:
+				Memory = document.createElement(Str);
+				break;
+		}
+		
+		return Memory;
+	}
+	
+	window.DOM.XHR = function (Args) {
+		let Connector = new XMLHttpRequest();
+			
+			Args.Params ? (function () {
+				let Param = [];
+				
+				for (let ParamName in Args.Params) {
+					Param.push(ParamName + "=" + Args.Params[ParamName]);
+				}
+				
+				Connector.open(Args.Type ? Args.Type : "GET", (Args.URL ? Args.URL : location.href) + "?" + Param.join("&"), Args.DoesSync ? Args.DoesSync : true);
+			})() : (function () {
+				Connector.open(Args.Type ? Args.Type : "GET", (Args.URL ? Args.URL : location.href), Args.DoesSync ? Args.DoesSync : true);
+			})();
+			
+			Args.Headers ? (function () {
+				for (let HeaderName in Args.Headers) {
+					Connector.setRequestHeader(HeaderName, Args.Headers[HeaderName]);
+				}
+			})() : (function () {
+				
+			})();
+			
+			Connector.onload = Args.OnLoad ? Args.OnLoad(Event) : null;
+			
+			Connector.send(Args.Data);
+	}
+	
+	window.DOM.JsonPXHR = function (Args) {
+		let Param = [];
+		
+		Args.Params ? (function () {
+			for (let ParamName in Args.Params) {
+				Param.push(ParamName + "=" + Args.Params[ParamName]);
+			}
+		})() : (function () {
+			
+		})();
+		
+		let Connector = document.createElement("Script");
+			Connector.src = (Args.URL ? Args.URL : location.href) + (Args.Params ? "?" + Param.join("&") : "");
+			
+			Connector.onload = function (Event) {
+				Connector.parentElement.removeChild(Connector);
+			}
+			
+		document.head.appendChild(Connector);
+	}
+	
+	
+	
+	window.DOM.width = window.innerWidth;
+	window.DOM.height = window.innerHeight;
+	
+	
+	
+	window.addEventListener("resize", function (Event) {
+		window.DOM.width = window.innerWidth;
+		window.DOM.height = window.innerHeight;
+	});
+})();
+
+(function () {
+	window.Script = function (Src, Options) {
+		let Elem = document.createElement("Script");
+			Elem.src = Src ? Src : "";
+			
+		Options ? (function () {
+			Options.Async ? Elem.setAttribute("Async", "") : null;
+			Options.Defer ? Elem.setAttribute("Defer", "") : null;
+		})() : (function () {
+			
+		})();
+		
+		return Elem;
+	}
+	
+	window.Svg = function (Width, Height) {
+		let Elem = document.createElement("Svg");
+			Elem.width = Width ? Width : "0";
+			Elem.height = Height ? Height : "0";
+			
+			Elem.setAttribute("Version", "1.1");
+			Elem.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+			
+		return Elem;
+	}
+	
+	window.Svg.Rect = function (Args) {
+		let Elem = document.createElement("Rect");
+			Elem.setAttribute("x", Args.X ? Args.X : "0");
+			Elem.setAttribute("y", Args.Y ? Args.Y : "0");
+			Elem.setAttribute("width", Args.Width ? Args.Width : "0");
+			Elem.setAttribute("height", Args.Height ? Args.Height : "0");
+			Elem.setAttribute("fill", Args.Fill ? Args.Fill : "#000000");
+			
+		return Elem;
+	}
+	
+	window.Svg.RGB = function (R, G, B) {
+		return "RGB(" + (R ? R : 0) + ", " + (G ? G : 0) + ", " + (B ? B : 0) + ")";
+	}
+	
+	window.Svg.RGBA = function (R, G, B, A) {
+		return "RGBA(" + (R ? R : 0) + ", " + (G ? G : 0) + ", " + (B ? B : 0) + ", " + (A ? A : 0) + ")";
+	}
+})();
+
+(function () {
+	window.location.querySort = function () {
+		var Querys = {};
+		
+		for (var i = 0; i < location.search.substr(1).split("&").length; i++) {
+			Querys[location.search.substr(1).split("&")[i].split("=")[0].toUpperCase()] = location.search.substr(1).split("&")[i].split("=")[1];
+		}
+		
+		return Querys;
+	}
+	
+	window.Image.prototype.getImageData = function () {
+		let Cvs = document.createElement("Canvas");
+			Cvs.width = this.naturalWidth;
+			Cvs.height = this.naturalHeight;
+			
+		let Ctx = Cvs.getContext("2d");
+			Ctx.drawImage(this, 0, 0);
+			
+		return Ctx.getImageData(0, 0, this.naturalWidth, this.naturalHeight);
+	}
+})();
