3d2e47fcdb5565af795ba5dddc2d7bf65dc8f816
Genbu Hase <hasegenbu@gmail.com>
2017-03-02 19:45:27 +0900

    とりまOAuth取得。

--- Content/PresentsForBirthday/Script/Main.js
+++ Content/PresentsForBirthday/Script/Main.js
@@ -1,8 +1,16 @@
 let Base = null;
+let GBase = null;
 
 window.addEventListener("load", function () {
     Base = new Chat({
         Theme: "LightSeaGreen",
         Parent: document.body
     }, true);
+
+    GBase = new GoogleAPI({
+        ID: "159240489618-dh7v838ef8b6eds5dtcr786bd6v7e52f.apps.googleusercontent.com",
+        Key: "P818It-akvSnfh8fOEqyZrXq",
+
+        Url: "http://localhost:8001/Content/PresentsForBirthday/"
+    });
 });
