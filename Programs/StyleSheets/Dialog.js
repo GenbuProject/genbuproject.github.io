6a867ca37e6decc5d66e70faa85d90c7a48b4e64
Genbu Hase <hasegenbu@gmail.com>
2017-04-02 17:24:34 +0900

    終了。

--- Programs/StyleSheets/Dialog.js
+++ Programs/StyleSheets/Dialog.js
@@ -1,33 +1,160 @@
-window["Dialog.js"] = {};
-window["Dialog.js"].HTMLDialogElement = Object.create(HTMLElement.prototype, {
-	title: { value: "" },
-	content: { value: "" },
-	theme: { value: "LightSeaGreen" },
-	width: { value: window.innerWidth / 3 },
-	height: { value: window.innerHeight / 1.5 },
-	root: { value: null },
+let self = this;
+
+self["Dialog.js"] = {};
+self["Dialog.js"].HTMLDialogElement = Object.create(HTMLElement.prototype, {
+	dialogTitle: { writable: true, enumerable: true },
+	dialogContent: { writable: true, enumerable: true },
+	dialogColor: { writable: true, enumerable: true },
+
+	width: { writable: true, enumerable: true },
+	height: { writable: true, enumerable: true },
+	root: { writable: true, enumerable: true },
+	footer: { writable: true, enumerable: true },
+
+	showed: { writable: true, enumerable: true },
 
 	show: {
 		value: function () {
-			this.root.appendChild(this);
-		},
-
-		configurable: false,
-		writable: false,
-		enumerable: false
+			if (!this.showed) {
+				this.applyConfig();
+				this.root.appendChild(this);
+			}
+		}
 	},
 
 	dismiss: {
 		value: function () {
-			this.parentElement.removeChild(this);
-		},
+			if (this.showed) {
+				this.parentElement.removeChild(this);
+			}
+		}
+	},
 
-		configurable: false,
-		writable: false,
-		enumerable: false
+	reset: {
+		value: function () {
+			this.dismiss();
+			this.show();
+		}
+	},
+
+	applyConfig: {
+		value: function () {
+			this.style.width = this.width + "px",
+			this.style.height = this.height + "px",
+			this.style.left = window.innerWidth / 2 - this.width / 2 + "px",
+			this.style.top = window.innerHeight / 2 - this.height / 2 + "px",
+
+			this.children[0].style.borderColor = this.dialogColor,
+			this.children[1].style.borderColor = this.dialogColor,
+			this.children[2].style.borderColor = this.dialogColor,
+
+			this.children[0].textContent = this.dialogTitle,
+			this.children[1].textContent = this.dialogContent;
+		}
+	},
+
+
+
+	createdCallback: {
+		value: function () {
+			this.dialogTitle = "",
+			this.dialogContent = "",
+			this.dialogColor = "DarkOrange",
+
+			this.width = window.innerWidth / 3,
+			this.height = window.innerHeight / 1.5,
+			this.root = document.body,
+			this.footer = new self["Dialog.js"].Dialog.Footer(),
+
+			this.showed = false;
+
+			this.appendChild(new self["Dialog.js"].Dialog.Title()),
+			this.appendChild(new self["Dialog.js"].Dialog.Content()),
+			this.appendChild(this.footer);
+
+			this.applyConfig();
+		}
+	},
+
+	attachedCallback: {
+		value: function () {
+			this.showed = true;
+		}
+	},
+
+	detachedCallback: {
+		value: function () {
+			this.showed = false;
+		}
+	},
+
+	attributeChangedCallback: {
+		value: function (Attr, OldVal, NewVal) {
+			
+		}
 	}
 });
 
-window["Dialog.js"].Dialog = document.registerElement("DialogJs-Dialog", {
-	prototype: window["Dialog.js"].HTMLDialogElement
+self["Dialog.js"].Dialog = document.registerElement("DialogJs-Dialog", {
+	prototype: self["Dialog.js"].HTMLDialogElement
 });
+
+self["Dialog.js"].Dialog.Title = document.registerElement("DialogJs-DialogTitle", {});
+self["Dialog.js"].Dialog.Content = document.registerElement("DialogJs-DialogContent", {});
+self["Dialog.js"].Dialog.Footer = document.registerElement("DialogJs-DialogFooter", {});
+
+
+
+self["Dialog.js"].appendStyle = function () {
+	if (!document.querySelector('Style[UUID="Dialog.js - DialogStyle"]')) {
+		let Style = document.createElement("Style");
+			Style.setAttribute("UUID", "Dialog.js - DialogStyle");
+
+			Style.textContent = [
+				"DialogJs-Dialog {",
+				"	Position: Fixed;",
+				"	Z-Index: 100;",
+				"	",
+				"	Display: Flex;",
+				"	Flex-Direction: Column;",
+				"}",
+				"",
+				"DialogJs-DialogTitle, DialogJs-DialogContent, DialogJs-DialogFooter {",
+				"	Border: Thin Solid Black;",
+				"}",
+				"",
+				"DialogJs-DialogTitle, DialogJs-DialogContent {",
+				"	Display: Block;",
+				"}",
+				"",
+				"DialogJs-DialogTitle {",
+				"	Height: 10%;",
+				"	",
+				"	Padding: 1em 1em 0 1em;",
+				"	Border-Radius: 1em 1em 0 0;",
+				"}",
+				"",
+				"DialogJs-DialogContent {",
+				"	Flex: 1;",
+				"	",
+				"	Padding: 1em;",
+				"}",
+				"",
+				"DialogJs-DialogFooter {",
+				"	Display: Flex;",
+				"	Flex-Direction: Row;",
+				"	Height: 10%;",
+				"}"
+			].join("\r\n");
+
+		document.head.appendChild(Style);
+	}
+};
+
+self["Dialog.js"].removeStyle = function () {
+	document.querySelector('Style[UUID="Dialog.js - DialogStyle"]') ? document.querySelector('Style[UUID="Dialog.js - DialogStyle"]').remove() : null;
+};
+
+
+
+self["Dialog.js"].appendStyle();
