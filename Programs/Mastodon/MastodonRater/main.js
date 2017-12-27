908808f1edf703400d44d25a3e48caa8cdc934e3
Genbu Hase <hasegenbu@gmail.com>
2017-12-27 16:54:24 +0900

    とりあえず成功。

--- Programs/Mastodon/MastodonRater/main.js
+++ Programs/Mastodon/MastodonRater/main.js
@@ -1,77 +1,20 @@
 class AppInfo {
-	static get Instances () {
-		return class Instances extends Array {
-			constructor () {
-				super();
-
-				if (!sessionStorage.getItem(KEYS.INSTANCES)) this.save();
-				this.load();
-			}
-
-			add (instance = "") {
-				if (!this.exist(instance)) {
-					this.push(instance);
-					this.save();
-				}
-			}
-
-			remove (instance = "") {
-				this.forEach((item, index) => {
-					if (instance == item) this.splice(index, 1);
-				});
-
-				this.save();
-			}
-
-			clear () {
-				while (this.length) this.splice(0, 1);
-			}
-
-			exist (instance = "") {
-				let hasInstance = false;
-
-				this.forEach(item => {
-					if (instance == item) hasInstance = true;
-				});
-
-				return hasInstance;
-			}
-
-			save (val) { sessionStorage.setItem(KEYS.INSTANCES, JSON.stringify(val || this || [])) }
-
-			load () {
-				this.clear();
-
-				let items = JSON.parse(sessionStorage.getItem(KEYS.INSTANCES));
-					items.forEach(instance => this.push(instance));
-			}
+	static get KEYS () {
+		return {
+			INSTANCE: "com.GenbuProject.MastodonRater.currentInstance",
+			ACCESSTOKEN: "com.GenbuProject.MastodonRater.accessToken"
 		}
 	}
 
-	static get instances () { return new AppInfo.Instances() }
 
 
+	constructor () {}
 
-	constructor () {
-		if (!AppInfo.instances) AppInfo.instances.add("");
-	}
-	
-	//.name(String)
-	//.website(String)
-	//.scopes(Array)
-	//.redirectUrl(String)
+	get instance () { return sessionStorage.getItem(AppInfo.KEYS.INSTANCE) }
+	set instance (url = "") { sessionStorage.setItem(AppInfo.KEYS.INSTANCE, url) }
 
-	get instance () { return sessionStorage.getItem(KEYS.INSTANCE) }
-	set instance (url = "") { sessionStorage.setItem(KEYS.INSTANCE, url) }
-
-	get clientId () { return sessionStorage.getItem(`${KEYS.CLIENTID}?${this.instance}`) }
-	set clientId (clientId = "") { sessionStorage.setItem(`${KEYS.CLIENTID}?${this.instance}`, clientId) }
-
-	get secretId () { return sessionStorage.getItem(`${KEYS.SECRETID}?${this.instance}`) }
-	set secretId (secretId = "") { sessionStorage.setItem(`${KEYS.SECRETID}?${this.instance}`, secretId) }
-
-	get accessToken () { return sessionStorage.getItem(`${KEYS.ACCESSTOKEN}?${this.instance}`) }
-	set accessToken (token = "") { sessionStorage.setItem(`${KEYS.ACCESSTOKEN}?${this.instance}`, token) }
+	get accessToken () { return sessionStorage.getItem(`${AppInfo.KEYS.ACCESSTOKEN}?${this.instance}`) }
+	set accessToken (token = "") { sessionStorage.setItem(`${AppInfo.KEYS.ACCESSTOKEN}?${this.instance}`, token) }
 }
 
 
@@ -83,35 +26,48 @@ const IDS = {
 		FORM: {
 			ROOT: "authPanel_authForm",
 
-			INSTANCE: "authPanel_authForm_instanceUrl-input",
+			INSTANCE: "authPanel_authForm_instanceUrl_items",
 			SUBMIT: "authPanel_authForm_submit"
 		}
 	},
 
 	CONTROL: {
-		ROOT: "controlPanel"
+		ROOT: "controlPanel",
+
+		APPS: {
+			ROOT: "controlPanel_apps",
+
+			TOOTRATER: "controlPanel_apps_app-tootRater",
+			REVELANCE: "controlPanel_apps_app-relevanceAnalyzer"
+		}
 	}
 }
 
-const KEYS = {
-	INSTANCE: "com.GenbuProject.MastodonRater.currentInstance",
-	CLIENTID: "com.GenbuProject.MastodonRater.clientId",
-	SECRETID: "com.GenbuProject.MastodonRater.secretId",
-	ACCESSTOKEN: "com.GenbuProject.MastodonRater.accessToken",
-	INSTANCES: "com.GenbuProject.MastodonRater.instances"
+const CLASSES = {
+	PANEL: {
+		ROOT: "mstdnRater_panel",
+		DISABLED: "mstdnRater_panel-disabled"
+	}
 }
 
-const DEFAULT = "https://mstdn.y-zu.org";
+const SERVERS = {
+	"https://mstdn.y-zu.org": {
+		CLIENTID: "bca80b2d45ed527ce2df258c221f9adfb6bf5deee793e7da8b6b024e0a3172cc",
+		SECRETID: "cb265cd91b565af0135544ba8e962f984da1f688ad6da44752de7988449600b0"
+	},
+
+	"https://mstdn.kemono-friends.info": {
+		CLIENTID: "0a23fb3bd0f024f1acc0fe28cda94f6a457b4189548e1bcabee31d3fe597a847",
+		SECRETID: "04a94354faf8885c8c5a70327e4d5ebec18856a5ecfa0961055ee70e41b24563"
+	}
+}
 
 
 
-let app = null,
-	appInfo = new AppInfo();
+let app = null;
 
-	appInfo.name = "MastodonRater";
-	appInfo.website = "https://genbuproject.github.io/Programs/Mastodon/MastodonRater/";
-	appInfo.scopes = ["read", "write"];
-
+let appInfo = new AppInfo();
+	appInfo.scopes = ["read", "write"],
 	appInfo.redirectUrl = (() => {
 		let url = new URL(location.href);
 			url.search = "";
@@ -126,71 +82,70 @@ window.addEventListener("DOMContentLoaded", () => {
 
 
 
-	let authForm = document.getElementById(IDS.AUTH.FORM.ROOT);
-	let instanceUrl = document.getElementById(IDS.AUTH.FORM.INSTANCE);
-
-	let authBtn = authForm.querySelector(`#${IDS.AUTH.FORM.SUBMIT}`);
-		authBtn.addEventListener("click", () => {
-			if (instanceUrl.checkValidity()) {
-				appInfo.instance = instanceUrl.value;
-				app = new MastodonAPI({ instance: appInfo.instance, api_user_token: "" });
-
-				app.registerApplication(appInfo.name, appInfo.redirectUrl, appInfo.scopes, appInfo.website, res => {
-					console.log(res);
-
-					appInfo.clientId = res.client_id,
-					appInfo.secretId = res.client_secret;
-
-					location.href = app.generateAuthLink(appInfo.clientId, appInfo.redirectUrl, "code", appInfo.scopes);
-				});
-			}
-		});
-
-
-		
 	let query = location.querySort();
 	
 	if (appInfo.accessToken) {
 		app = new MastodonAPI({ instance: appInfo.instance, api_user_token: appInfo.accessToken });
+
+		document.getElementById(IDS.AUTH.ROOT).classList.toggle(CLASSES.PANEL.DISABLED),
+		document.getElementById(IDS.CONTROL.ROOT).classList.toggle(CLASSES.PANEL.DISABLED);
 	} else if (query.CODE) {
 		app = new MastodonAPI({ instance: appInfo.instance });
 
-		app.getAccessTokenFromAuthCode(appInfo.clientId, appInfo.secretId, appInfo.redirectUrl, query.CODE, res => {
+		app.getAccessTokenFromAuthCode(SERVERS[appInfo.instance].CLIENTID, SERVERS[appInfo.instance].SECRETID, appInfo.redirectUrl, query.CODE, res => {
 			console.log(res);
 
-			AppInfo.instances.add(appInfo.instance);
 			appInfo.accessToken = res.access_token;
-			app.setConfig("api_user_token", appInfo.accessToken);
+			location.href = appInfo.redirectUrl;
 		});
-	} else {
-		instanceUrl.value = appInfo.instance = DEFAULT;
 	}
 });
 
-/*
-let serverInfo = JSON.parse(
-	DOM.xhr({
-		type: "GET",
-		url: "/api/v1/instance"
-	}).response
-);
+window.addEventListener("DOMContentLoaded", () => {
+	let authForm = document.getElementById(IDS.AUTH.FORM.ROOT);
+		authForm.querySelector(`#${IDS.AUTH.FORM.SUBMIT}`).addEventListener("click", () => {
+			let instanceUrl = authForm.querySelector(`#${IDS.AUTH.FORM.INSTANCE}`);
+			
+			if (instanceUrl.checkValidity()) {
+				appInfo.instance = instanceUrl.value;
+				app = new MastodonAPI({ instance: appInfo.instance });
 
-let userInfo = JSON.parse(
-	DOM.xhr({
-		type: "GET",
-		url: "/api/v1/accounts/verify_credentials",
+				location.href = app.generateAuthLink(SERVERS[appInfo.instance].CLIENTID, appInfo.redirectUrl, "code", appInfo.scopes);
+			}
+		});
 
-		params: { "access_token": sessionStorage.getItem("com.GenbuProject.MastodonRater.accessToken") }
-	}).response
-);
+	let apps = document.getElementById(IDS.CONTROL.APPS.ROOT);
+		apps.querySelector(`#${IDS.CONTROL.APPS.TOOTRATER}`).addEventListener("click", (event) => {
+			event.preventDefault();
 
-let serverToots = serverInfo.stats.status_count,
-	userToots = userInfo.statuses_count;
+			let serverInfo = {},
+				userInfo = {};
 
-contents.value = [
-	"#トゥート率",
-	`@${userInfo.username} さんの`,
-	`トゥート率は${(userToots / serverToots * 100).toFixed(2)}%です！`,
-	`(Tooted from UtilBtns ${VERSION})`
-].join("\r\n");
-*/
+				app.get("instance").then(res => serverInfo = res).then(res => {
+					app.get("accounts/verify_credentials").then(res => userInfo = res).then(res => {
+						let serverToots = serverInfo.stats.status_count,
+							userToots = userInfo.statuses_count;
+							
+						app.post("statuses", {
+							status: [
+								"#トゥート率",
+								`@${userInfo.username} さんの`,
+								`トゥート率は${(userToots / serverToots * 100).toFixed(2)}%です！`,
+								"(Tooted from MastodonRater)"
+							].join("\r\n")
+						});
+					});
+				});
+		});
+
+	
+	let query = location.querySort();
+	
+	if (!appInfo.accessToken && !query.CODE) {
+		let instances = authForm.querySelector(`#${IDS.AUTH.FORM.INSTANCE}`);
+
+		for (let server in SERVERS) {
+			instances.add(new Option(server, server));
+		}
+	}
+});
