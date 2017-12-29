aab9ffceee291266fbd297333940ff1afc3e5290
Genbu Hase <hasegenbu@gmail.com>
2017-12-29 09:47:44 +0900

    [テスト] Notify変更

--- Programs/Mastodon/MastodonRater/classes/Notify.js
+++ Programs/Mastodon/MastodonRater/classes/Notify.js
@@ -2,7 +2,7 @@ class Notify {
 	constructor (snackbar) {
 		this.self = new mdc.snackbar.MDCSnackbar(snackbar);
 
-		Notification.requestPermission(result => {
+		/*Notification.requestPermission(result => {
 			switch (result) {
 				case "denied":
 					alert([
@@ -12,7 +12,7 @@ class Notify {
 
 					break;
 			}
-		});
+		});*/
 	}
 
 	begin () {
@@ -20,11 +20,11 @@ class Notify {
 			message: "処理を実行しています..."
 		});
 
-		let notify = new Notification("MastodonRater", {
+		/*let notify = new Notification("MastodonRater", {
 			body: "処理を実行しています..."
 		});	notify.addEventListener("click", function () {
 			this.close();
-		});
+		});*/
 	}
 
 	finish () {
@@ -32,10 +32,10 @@ class Notify {
 			message: "投稿が反映されました"
 		});
 
-		let notify = new Notification("MastodonRater", {
+		/*let notify = new Notification("MastodonRater", {
 			body: "投稿が反映されました"
 		});	notify.addEventListener("click", function () {
 			this.close();
-		});
+		});*/
 	}
 }
