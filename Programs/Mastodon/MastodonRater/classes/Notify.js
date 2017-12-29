5470b94d49c18891495e50c1a0f227c6a21552d5
Genbu Hase <hasegenbu@gmail.com>
2017-12-29 09:30:49 +0900

    Notify改善。

--- Programs/Mastodon/MastodonRater/classes/Notify.js
+++ Programs/Mastodon/MastodonRater/classes/Notify.js
@@ -2,7 +2,17 @@ class Notify {
 	constructor (snackbar) {
 		this.self = new mdc.snackbar.MDCSnackbar(snackbar);
 
-		Notification.requestPermission();
+		Notification.requestPermission(result => {
+			switch (result) {
+				case "denied":
+					alert([
+						"通知認証が拒否されています。",
+						"アプリの動作に支障が出る恐れがあります。"
+					].join("\r\n"));
+
+					break;
+			}
+		});
 	}
 
 	begin () {
@@ -10,7 +20,11 @@ class Notify {
 			message: "処理を実行しています..."
 		});
 
-		new Notification("処理を実行しています...");
+		let notify = new Notification("MastodonRater", {
+			body: "処理を実行しています..."
+		});	notify.addEventListener("click", function () {
+			this.close();
+		});
 	}
 
 	finish () {
@@ -18,6 +32,10 @@ class Notify {
 			message: "投稿が反映されました"
 		});
 
-		new Notification("投稿が反映されました");
+		let notify = new Notification("MastodonRater", {
+			body: "投稿が反映されました"
+		});	notify.addEventListener("click", function () {
+			this.close();
+		});
 	}
 }
