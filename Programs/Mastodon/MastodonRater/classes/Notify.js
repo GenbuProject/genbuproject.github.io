0b9fac817d2b3fc5f74ce2727301a81431adb986
Genbu Hase <hasegenbu@gmail.com>
2017-12-29 10:30:58 +0900

    一旦通知機能を削除。

--- Programs/Mastodon/MastodonRater/classes/Notify.js
+++ Programs/Mastodon/MastodonRater/classes/Notify.js
@@ -2,19 +2,17 @@ class Notify {
 	constructor (snackbar) {
 		this.self = new mdc.snackbar.MDCSnackbar(snackbar);
 
-		if (Notification) {
-			Notification.requestPermission(result => {
-				switch (result) {
-					case "denied":
-						alert([
-							"通知認証が拒否されています。",
-							"アプリの動作に支障が出る恐れがあります。"
-						].join("\r\n"));
+		/*Notification.requestPermission(result => {
+			switch (result) {
+				case "denied":
+					alert([
+						"通知認証が拒否されています。",
+						"アプリの動作に支障が出る恐れがあります。"
+					].join("\r\n"));
 
-						break;
-				}
-			});
-		}
+					break;
+			}
+		});*/
 	}
 
 	begin () {
@@ -22,13 +20,11 @@ class Notify {
 			message: "処理を実行しています..."
 		});
 
-		if (Notification) {
-			let notify = new Notification("MastodonRater", {
-				body: "処理を実行しています..."
-			});	notify.addEventListener("click", function () {
-				this.close();
-			});
-		}
+		/*let notify = new Notification("MastodonRater", {
+			body: "処理を実行しています..."
+		});	notify.addEventListener("click", function () {
+			this.close();
+		});*/
 	}
 
 	finish () {
@@ -36,12 +32,10 @@ class Notify {
 			message: "投稿が反映されました"
 		});
 
-		if (Notification) {
-			let notify = new Notification("MastodonRater", {
-				body: "投稿が反映されました"
-			});	notify.addEventListener("click", function () {
-				this.close();
-			});
-		}
+		/*let notify = new Notification("MastodonRater", {
+			body: "投稿が反映されました"
+		});	notify.addEventListener("click", function () {
+			this.close();
+		});*/
 	}
 }
